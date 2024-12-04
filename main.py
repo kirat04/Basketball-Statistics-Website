01e6b47f86f3fd9a5f9d1a1057a083d11b176e3a
kirat <dhalkirat1@gmail.com>
2024-12-03 23:58:23 -0500

    implementation done, add data to database

--- main.py
+++ main.py
@@ -25,15 +25,23 @@ def index():
 def search():
     if request.method == 'POST':
         search_query = request.form['query']
-        team_query = int(request.form['team'])
+        team_query = int(request.form['team'])-1
+        if team_query == -1:
+            team_query = 0
+
+        year = int(request.form['year'])
+        print(year)
+        
+        points = int(request.form['points'])
+        print(points)
         print("Search request received:" + teams[team_query] + " " + search_query)
         conn = sqlite3.connect('database.db')
         c = conn.cursor()
-        #find the all players that have this name if no team specified
+        #find the all players that have this name if no team specified else use the team in the query
         if(team_query == 0):
-            c.execute("SELECT name FROM Players WHERE name LIKE ?", ('%' + search_query + '%',))
+            c.execute("SELECT name,player_ID FROM Players WHERE name LIKE ?", ('%' + search_query + '%',))
         else:
-            c.execute('''SELECT Players.name
+            c.execute('''SELECT Players.name, Players.player_ID
                          FROM Players
                          JOIN Played_In ON Players.player_ID = Played_In.player
                          JOIN Games ON Played_In.game = Games.game_Id
@@ -43,21 +51,47 @@ def search():
         results = c.fetchall()
         results = list(results)
         print(results)
+        #for each player, get the games they played in
+        game_results = []
+        if year == 0:
+            for result in results:
+                c.execute('''SELECT Games.game_Id, Games.Date, Games.season_played, Played_In.points
+                             FROM Played_In
+                             JOIN Players ON Played_In.player = Players.player_ID
+                             JOIN Games ON Played_In.game = Games.game_Id
+                             WHERE Players.name = ? 
+                               AND Played_In.points > ?''', (result[0], points))
+                game_results.extend(c.fetchall())
+        else:
+            for result in results:
+                c.execute('''SELECT Games.game_Id, Games.Date, Games.season_played, Played_In.points
+                            FROM Played_In
+                            JOIN Players ON Played_In.player = Players.player_ID
+                            JOIN Games ON Played_In.game = Games.game_Id
+                            WHERE Players.name = ? 
+                            AND Games.season_played = ? 
+                            AND Played_In.points > ?''', (result[0], year, points))
+                game_results.extend(c.fetchall())
+        print(game_results)
         conn.close()
         return render_template('search.html', items=results)
     return render_template('search.html', results=[])
 
-# @app.route('/player/<int:player_id>')
-# def player(player_id):
-#     conn = sqlite3.connect('database.db')
-#     c = conn.cursor()
-#     c.execute("SELECT * FROM Players WHERE id = ?", (player_id,))
-#     player = c.fetchone()
-#     conn.close()
-#     if player:
-#         return render_template('player.html', player=player)
-#     else:
-#         return "Player not found", 404
+@app.route('/player/<int:player_id>')
+def player(player_id):
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute('''SELECT Players.name, Games.game_Id, Games.Date, Games.season_played, Played_In.points, Played_In.rebounds, Played_In.assists, Played_In.steals, Played_In.blocks
+                FROM Played_In
+                JOIN Players ON Played_In.player = Players.player_ID
+                JOIN Games ON Played_In.game = Games.game_Id
+                WHERE Players.player_ID = ?''', (player_id,))
+    player = c.fetchall()
+    conn.close()
+    if player:
+        return render_template('player.html', player=player)
+    else:
+        return "Player not found", 404
 
 if __name__ == '__main__':
     # init_db()
